7ed1ae0c8338f5acc40228c8b4fe2b03c192ad7a
Leandro <Leandro Santos>
2022-11-24 11:51:46 -0300

    Add project files.

---
+++ Testador/Form1.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Testador.Objeto;
+
+namespace Testador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.WebServiceSoapSoapClient objeto = new ServiceReference1.WebServiceSoapSoapClient();
+
+            try
+            {
+                richTextBox1.AppendText(objeto.Consumindo());
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.AppendText(ex.Message + "\n");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Teste();
+        }
+
+        private async void Teste()
+        {
+            string url = "https://localhost:44386/weatherforecast";
+
+            JsonSerializerSettings settings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    using (var response = await client.GetAsync(url))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var ProdutoJsonString = await response.Content.ReadAsStringAsync();
+                            IList<WeatherForecast> objeto = JsonConvert.DeserializeObject<WeatherForecast[]>(ProdutoJsonString).ToList();
+
+                            richTextBox1.Text = JsonConvert.SerializeObject(objeto, settings) + "\n";
+
+                        }
+                        else
+                        {
+                            richTextBox1.AppendText("Não foi possível obter o produto : " + response.StatusCode + "\n");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    richTextBox1.AppendText(ex.Message + "\n");
+                }
+            }
+        }
+    }
+}
---
+++ Testador/Objeto/WeatherForecast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testador.Objeto
+{
+
+    public class WeatherForecast
+    {
+        public DateTime date { get; set; }
+        public int temperatureC { get; set; }
+        public int temperatureF { get; set; }
+        public string summary { get; set; }
+    }
+
+}
